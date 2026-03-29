65db9c957c7a73d74a242066b563f016e4fc1e16
Mahesh1325 <mmaheshbala2005@gmail.com>
2026-03-29 14:17:48 +0530

    future tailwind feature if needed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    "./*.html",
+    "./pages/**/*.html",
+    "./assets/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
